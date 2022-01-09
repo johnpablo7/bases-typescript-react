959fabd98db126ae49fe556e7b08d272502cb77d
john <john.davis.2015.03@gmail.com>
2022-01-08 19:57:31 -0500

    useRef y TimeLines, CustomHooks con referencias HTML

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -3,7 +3,7 @@ import { gsap } from "gsap";
 
 const MAXIMUN_COUNT = 10;
 
-export const CounterEffect = () => {
+export const useCounter = () => {
   const [counter, setCounter] = useState(5);
   const counterElement = useRef<HTMLHeadingElement>(null);
 
@@ -19,16 +19,14 @@ export const CounterEffect = () => {
     );
 
     const tl = gsap.timeline();
+
     tl.to(counterElement.current, { y: -10, duration: 0.2, ease: "ease.out" });
     tl.to(counterElement.current, { y: 0, duration: 1, ease: "bounce.out" });
   }, [counter]);
 
-  return (
-    <div>
-      <h1>CounterEffect: </h1>
-      <h2 ref={counterElement}>{counter}</h2>
-
-      <button onClick={handleClick}>+1</button>
-    </div>
-  );
+  return {
+    counter,
+    counterElement,
+    handleClick,
+  };
 };
